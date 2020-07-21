b1f86923f8c1afe15fbe8f23721cf566d8b95f44
SofianSaleh <sofiane.saleh11@gmail.com>
2020-07-21 12:33:04 +0200

    :rocket: init

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-import { ApolloServer } from "apollo-server-express";
-import express from "express";
-import chalk from "chalk";
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
 
-const PORT = process.env.PORT || 8000;
+createConnection().then(async connection => {
 
-const main = () => {
-  const app = express();
-  app.use(express.json());
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await connection.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
 
-  const apolloServer = new ApolloServer({});
+    console.log("Loading users from the database...");
+    const users = await connection.manager.find(User);
+    console.log("Loaded users: ", users);
 
-  apolloServer.applyMiddleware({ app });
+    console.log("Here you can setup and run express/koa/any other framework.");
 
-  app.listen(PORT, () =>
-    console.log(chalk.blue(`Server listening on port: ${chalk.yellow(PORT)}`))
-  );
-};
-
-main();
+}).catch(error => console.log(error));
