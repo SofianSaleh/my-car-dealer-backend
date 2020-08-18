e8325c3667702dfd1b71ac24266f232423cdafbd
SofianSaleh <sofiane.saleh11@gmail.com>
2020-08-18 12:26:34 +0200

    :fire: subscription table

---
+++ src/db/entity/UserStats.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  ManyToOne,
+  Column,
+} from 'typeorm';
+import { ObjectType, Field, ID } from 'type-graphql';
+
+import { User } from './User';
+
+@ObjectType()
+@Entity()
+export class User_Stats extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Field()
+  @ManyToOne((_type) => User, (user) => user.id)
+  user_id: User;
+
+  @Field()
+  @Column({ type: 'integer', nullable: true, default: 0 })
+  cars_sold: number;
+
+  @Field()
+  @Column({ type: 'integer', nullable: true, default: 0 })
+  cars_bought: number;
+}
