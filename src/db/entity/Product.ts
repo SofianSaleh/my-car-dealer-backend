7415f6b1809891d0a13cd76f7e6da73c4493bedd
SofianSaleh <sofiane.saleh11@gmail.com>
2020-08-18 11:53:54 +0200

    :fire: finshed cars table

---
+++ src/db/entity/Product.ts
@@ -0,0 +1,31 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
+import { ObjectType, Field, ID } from 'type-graphql';
+
+@ObjectType()
+@Entity()
+export class Product extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Field()
+  @Column('text', { nullable: true })
+  title: string;
+
+  @Field()
+  @Column('text', { nullable: true })
+  description: string;
+
+  @Field()
+  @Column('text', { nullable: true })
+  price: string;
+
+  @Field()
+  @Column('text', { nullable: true })
+  location: string;
+
+  // @Field()
+  // name(@Root() parent: Product): string {
+  //   return `${parent.firstName} ${parent.lastName}`;
+  // }
+}
